6f847bb88fb379e5eda2690c091452c7979681d5
Paul Jensen <paul.jensen@bc.edu>
2015-07-06 15:21:54 -0400

    fixed locked environment error

--- R/report.R
+++ R/report.R
@@ -1,37 +1,42 @@
 
-current_log_file <- NULL
-indents <- c()
+reportEnv <- new.env()
+
+assign("current_log_file", NULL, envir=reportEnv)
+assign("indents", c(), envir=reportEnv)
 
 set_log_file <- function(filename) {
-  current_log_file <<- filename
+  assign("current_log_file", filename, envir=reportEnv)
 }
 
 start_log_file <- function(filename) {
-  current_log_file <<- filename
+  set_log_file(filename)
   close(file(filename, open="w"))
 }
 
 append_log_file <- function(file) {
-  current_log_file <<- file
+  set_log_file(file)
 }
 
 clear_log_file <- function() {
-  current_log_file <<- NULL
+  set_log_file(NULL)
 }
 
 indent_report <- function(indent="   ") {
-  indents <<- c(indents, indent)
+  assign("indents", c(get("indents", envir=reportEnv), indent), envir=reportEnv)
 }
 
 unindent_report <- function(n=1) {
+  indents <- get("indents", envir=reportEnv)
   if (length(indents) - n < 1) {
-    indents <<- c()
+    assign("indents", c(), envir=reportEnv)
   } else {
-    indents <<- indents[1:(length(indents)-n)]
+    assign("indents", indents[1:(length(indents)-n)], envir=reportEnv)
   }
 }
 
 report <- function(string, tofile=T) {
+  indents <- get("indents", envir=reportEnv)
+  current_log_file <- get("current_log_file", envir=reportEnv)
   string <- paste0(paste(indents, collapse=""), string, "\n")
   cat(string)
   if (tofile && !is.null(current_log_file)) {
