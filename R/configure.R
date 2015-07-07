e71879eea649c98b9e55f3e319af4d4d3be709f3
Paul Jensen <paul.jensen@bc.edu>
2015-07-07 13:26:28 -0400

    fixed exports for parallel configs

--- R/configure.R
+++ R/configure.R
@@ -10,16 +10,22 @@ NULL
 
 #' @describeIn parallel Set the number of cores.  Set to 1 to turn off
 #' parallelization.
+#' 
+#' @export
 use_multiple_cores <- function(ncores) {
   options(mc.cores=ncores)
 }
 
 #' @describeIn parallel Alias for use_multiple_cores(1).
+#' 
+#' @export
 use_single_core <- function() {
   use_multiple_cores(1)
 }
 
 #' @describeIn parallel Return the number of cores to be used.
+#' 
+#' @export
 get_cores <- function() {
   getOption("mc.cores")
 }
