633f43342d7419bd456ef5af35af16af477cd86d
Paul Jensen <paul.jensen@bc.edu>
2015-09-09 00:33:03 -0500

    add annotations to reports

--- R/generate.R
+++ R/generate.R
@@ -1,12 +1,26 @@
 
+get_genome_feature_table <- function(tnseq) {
+  dfs <- lapply(tnseq$features, function(x) data.frame(gene=names(x),
+                                                       name=x$name,
+                                                       product=x$product))
+  df <- do.call(rbind, dfs)
+  rownames(df) <- df$gene
+  df$gene <- NULL
+  return(df)
+}
+
 #' @export
 generate_reports <- function(tnseq) {
+  lookup <- get_genome_feature_table(tnseq)
+  
   readr::write_csv(tnseq$insertions, 
                    path=get_path(tnseq, dir="reports", file="insertions.csv"))
 
   writer <- function(df) {
     file <- paste(df$strain, df$library, df$condition)
     file <- paste0(file, ".csv")
+    df <- df[!is.na(df$gene), ]
+    df <- cbind(df, lookup[df$gene, ])
     readr::write_csv(df, path=get_path(tnseq, dir="reports", file=file)[1])
     return(df)
   }
